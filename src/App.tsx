d8044e36f89b2cb5917f87d957251a803cfb493d
MRloll <w.abdelkrim@wakeb.tech>
2026-09-05 20:12:28 +0300

    feat(nav)
    - add navbar
    - add assets
    - animate navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    <div>
-      <h1 className="flex-center h-[100vh]">Hello gsap</h1>
-    </div>
+    <main>
+      <Navbar />
+      <h1>asdasd</h1>
+    </main>
   );
 }
 
